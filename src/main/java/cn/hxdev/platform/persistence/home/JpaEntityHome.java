8eb35161a872197141ae27732c805272126b57c3
myhtl <myhtls@163.com>
2016-05-24 09:54:47 +0800

    增加了一个EntityHome接口,一个AbstractEntityHome抽象类,一个JpaEntityHome

---
+++ src/main/java/cn/hxdev/platform/persistence/home/JpaEntityHome.java
@@ -0,0 +1,45 @@
+package cn.hxdev.platform.persistence.home;
+
+import javax.persistence.EntityManager;
+
+/**
+ * Created by myhtls on 16/5/24.
+ */
+public class JpaEntityHome<T> extends AbstractEntityHome<T> {
+
+    private EntityManager entityManager;
+
+    public EntityManager getEntityManager() {
+        return entityManager;
+    }
+
+    public void setEntityManager(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    protected T doLoadEntity() {
+        return (T) getEntityManager().find(getEntityClass(), getId());
+    }
+
+    @Override
+    protected void doInsert() {
+        getEntityManager().persist(getEntity());
+    }
+
+    @Override
+    protected void doCancel() {
+        getEntityManager().refresh(getEntity());
+    }
+
+
+
+
+    @Override
+    protected void doDelete() {
+        T t = doLoadEntity();
+        if (t != null) {
+            getEntityManager().remove(t);
+        }
+    }
+}
